dd8a01299c92fed1fbc39038388657de27b73293
Patrick Dukes <pdukes3@gmail.com>
2025-11-13 02:38:39 -0500

    refactor: standardize quote style in vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: "127.0.0.1", // don’t bind to 0.0.0.0
+    host: '127.0.0.1', // don’t bind to 0.0.0.0
     strictPort: true,
     cors: false, // disable CORS on dev server
-    origin: "http://localhost:5173",
+    origin: 'http://localhost:5173',
   },
   resolve: {
     alias: {
-      "@": "./src",
+      '@': './src',
     },
   },
-});
+})
